18d1d8c26e414ca724f20d7a9861d02e2687b98e
Nishworld <nishantdevkhile1@gmail.com>
2026-09-01 21:38:13 +0530

    added FastAPI backend and updated requirements

--- restraurant.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-sqlite3.connect("restraurant.db")
-q1 = """ create table menu (
-         id int primary key , 
-         name varchar(50) not FULL 
-         price number(50)
-         """
-q2 = """ create table   ""
